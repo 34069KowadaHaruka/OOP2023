c0be747ff0aee93cb051e1965e396e94f0a025e3
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-08 12:02:13 +0900

    0508演習2

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -15,16 +15,34 @@ namespace ProductSample {
             //    Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
             //    Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());
             #endregion
+            #region //0508演習1
+            //DateTime date = DateTime.Today;
 
-            DateTime date = DateTime.Today;
+            ////10日後を求める
+            //DateTime daysAfter10 = date.AddDays(10);
+            //DateTime daysBefore10 = date.AddDays(-10);
 
-            //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
-            DateTime daysBefore10 = date.AddDays(-10);
+            //Console.WriteLine("今日の日付：" + date.ToString("yyyy年MM月dd日"));
+            //Console.WriteLine("10日後：" + daysAfter10.ToString("yyyy年MM月dd日"));
+            //Console.WriteLine("10日前：" + daysBefore10.ToString("yyyy年MM月dd日"));
+            #endregion
 
-            Console.WriteLine("今日の日付：" + date.ToString("yyyy年MM月dd日"));
-            Console.WriteLine("10日後：" + daysAfter10.ToString("yyyy年MM月dd日"));
-            Console.WriteLine("10日前：" + daysBefore10.ToString("yyyy年MM月dd日"));
+            Console.WriteLine("誕生日を入力");
+            Console.Write("西暦：");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            DateTime birthDay = new DateTime(year, month, day);
+            DateTime toDay = DateTime.Today;
+
+            TimeSpan interval = toDay - birthDay;
+
+            Console.WriteLine();
+            Console.WriteLine("あなたは生まれてから今日で" + interval.TotalDays + "日です。");
         }
     }
 }
