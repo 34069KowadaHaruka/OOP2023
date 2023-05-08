473f8cff41d329a4124657f59d5a77147d5f99a2
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-08 10:54:14 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    class Program {
+        static void Main(string[] args) {
+            //インスタンス(オブジェクト)の生成
+            Product karinto = new Product (123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
+            //税込価格の表示
+            Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
+            Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());
+        }
+    }
+}
