ec5b576d108bf077238f41ecf1ae55affabaebdc
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-10 11:25:11 +0900

    距離換算プログラム 完成

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -8,14 +8,17 @@ namespace DistanceConverter {
     //フィートとメートルの単位変換クラス
     public static class FeetConverter { //staticメソッドしかない→staticクラスにしておけばインスタンスを作ろうとしたときにエラーが出るので、他開発者に向けたプログラムとして良い
 
+        private const double ratio = 0.3048; //T:ratioはprivateだから全部小文字なんだと思う //T:constって書くと自動的にstaticになります
+        //public static readonly double ratio = 0.3048; //p.58
+
         //メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3408;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
 
     }
