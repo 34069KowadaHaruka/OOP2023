1a3c75b9ad0fcc84fc35d41b23c516cc2e83fe73
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-16 10:21:37 +0900

    ReadSalesメソッドをSalesCounterクラスへ移動 (模範解答前)

--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,29 @@ namespace SalesCalculator {
         //アクセス修飾子の有効範囲をもう一度確認した方がよさそうですね……
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>(); //売上データを格納する
+            string[] lines = File.ReadAllLines(filePath); //ファイルからすべてのデータを読み込む
+
+            foreach (string line in lines) //すべてのデータ行から1行ずつ順に取り出す
+            {
+                string[] items = line.Split(','); //,区切りで項目別に分ける
+                Sale sale = new Sale { //Saleオブジェクト(インスタンス)を生成
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale); //Saleインスタンスをコレクションに追加
+            }
+            //Sale型のList salesの要素1つにはSaleオブジェクト1つ(つまり要素3つの配列)が入っている
+            return sales;
+        }
+
 
         //店舗別売上を求める
         public Dictionary<string, int> GetPerStoreSales() {
@@ -21,7 +42,8 @@ namespace SalesCalculator {
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
                     dict[sale.ShopName] += sale.Amount; //店名が既に存在する(売上加算)
-                }else {
+                }
+                else {
                     dict[sale.ShopName] = sale.Amount; //店名が存在しない(新規格納)
                 }
             }
