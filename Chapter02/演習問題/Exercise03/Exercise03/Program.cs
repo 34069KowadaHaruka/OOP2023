ab8145c626e3dc0477177d13e0e81d614a21a545
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-22 10:18:45 +0900

    問題2.3 I/F変更

--- Chapter02/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -8,10 +8,31 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var sales = new SalesCounter(@"data\Sales.csv");
-            var amountPerStore = sales.GetPerStoreSales();
-            foreach (var obj in amountPerStore) {
-                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
-            }
+            Console.WriteLine("**売上集計**");
+            Console.WriteLine("1:店舗別売上");
+            Console.WriteLine("2:商品カテゴリ別売上");
+            Console.Write("＞ ");
+            int cursor = int.Parse(Console.ReadLine());
+            do {
+                if (cursor == 1) {
+                    var amountPerStore = sales.GetPerStoreSales();
+                    foreach (var obj in amountPerStore) {
+                        Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+                    }
+                    break;
+                }
+                else if (cursor == 2) {
+                    var amountPerCategory = sales.GetPerCategorySales();
+                    foreach (var obj in amountPerCategory) {
+                        Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+                    }
+                    break;
+                }
+                Console.WriteLine("正しい数ではありません。再入力してください。");
+                Console.Write("＞ ");
+                cursor = int.Parse(Console.ReadLine());
+            } while (cursor != 1 && cursor != 2);
+            
         }
     }
 }
