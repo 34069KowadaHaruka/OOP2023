cb0c11a9ef95699fb506ede42420a90d532c81e9
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-22 11:58:33 +0900

    ラムダ式以前（デリゲート、匿名メソッド）

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace Section01 {
     class Program {
+
+        //public delegate bool Judgement(int value); //デリゲートの宣言
+        //Judgement型のdelegate みたいなイメージかも
+
         static void Main(string[] args) {
-            Console.WriteLine(Count(5));
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
+            //Judgement judge = IsEven; 
+
+            int count = Count(numbers, delegate(int n) { return n % 2 == 0; });
+            Console.WriteLine(count);
         }
 
-        public static int Count(int num) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
+        //public static bool IsEven(int n) {
+        //    return n % 2 != 0;
+        //}
+
+        public static int Count(int[] numbers, Predicate<int> judge) {
             int count = 0;
-            foreach (int n in numbers) {
-                if (n == num) {
+            foreach (var n in numbers) {
+                if (judge(n) == true) {
                     count++;
                 }
             }
