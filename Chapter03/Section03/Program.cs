d7e9c79a6cf710ea60c3e7ee5accd71cafc0b0e5
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-23 11:12:23 +0900

    p90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -8,10 +8,28 @@ namespace Section03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
-               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+               "Tokyo", "New Delhi", "Bangkok", "London", 
+               "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+            #region p90まで
+            //var exists = list.Exists(s => s[2] == 'k');
+            //Console.WriteLine(exists);
 
+            //var name = list.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
 
+            //var index = list.FindIndex(s => s.Length == 6);
+            //var index = list.FindIndex(s => s == "Hong Kong");
+            //Console.WriteLine(index);
+
+            //var names = list.FindAll(s => s.Length <= 5);
+            //var names = list.ConvertAll(s => s.ToLower());
+            //foreach (var name in names) {
+            //    Console.WriteLine(name);
+            //}
+            //names.ForEach(s => Console.WriteLine(s)); //names.ForEach(Console.WriteLine); //←可
+
+            #endregion
         }
     }
 }
