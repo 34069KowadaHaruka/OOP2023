5890f90396a5d3e53ce0800d3aff121cc5e8763b
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-05-29 10:53:56 +0900

    条件演算子

---
+++ Chapter04/条件演算子・null合体演算子/Section04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<int> {10,20,30,4, };
+            var key = 40;
+
+            var num = list.Contains(key) ? 1 : 0;
+            Console.WriteLine(num);
+        }
+    }
+}
