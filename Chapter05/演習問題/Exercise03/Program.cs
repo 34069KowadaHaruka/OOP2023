6e2649c05a732007898d11a310fa8f01fc71c969
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-06-05 09:41:21 +0900

    第５章　演習問題３　５問め

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -50,7 +50,12 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_5(string text) {
-            throw new NotImplementedException();
+            string[] words = text.Split(' ');
+            var sb = new StringBuilder();
+            foreach (var word in words) {
+                sb.Append(word + " ");
+            }
+            Console.WriteLine(sb);
         }
     }
 }
