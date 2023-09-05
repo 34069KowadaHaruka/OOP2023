a64ffb4c9a3354136de925af7243ccb083a331a1
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-09-05 14:07:43 +0900

    Singleton 適用

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -5,9 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CarReportSystem {
-    //設定情報
+    //設定情報 ..Singletonで実装
     public class Settings {
         public int MainFormColor { get; set; } //クラス型を直接シリアル化するため、Color構造体でなくint
-        
+        //自分自身のインスタンスを入れる変数
+        private static Settings instance;
+
+        //コンストラクタ
+        private Settings(){}
+        public static Settings getInstance() {
+            if (instance == null) { //instanceがnullならインスタンスを作ってinstanceに入れ、既にあるなら何もしない
+                instance = new Settings();
+            }
+            return instance; //instance変数を返す
+        }
     }
 }
