efdf7169876d8a7350c3b0d3c21196807836159b
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-09-14 09:49:38 +0900

    13章　演習前まで

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -8,10 +8,67 @@ using System.Threading.Tasks;
 namespace SampleEntityFramework {
     class Program {
         static void Main(string[] args) {
-            InsertBooks();
-            Console.Write("データを挿入しました。続けるにはEnterキーを押してください。");
+            //InsertBooks();
+
+            //DisplayAllBooks();
+
+            //AddAuthors();
+
+            //AddBooks();
+            //Console.Write("データを挿入しました。続けるにはEnterキーを押してください。");
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+            using (var db = new BooksDbContext()){
+                var count = db.Books.Count();
+                Console.WriteLine(count);
+            }
+        }
+
+        private static void AddBooks() {
+            using (var db = new BooksDbContext()) {
+                var author1 = db.Authors.Single(a => a.Name == "与謝野晶子");
+                var book1 = new Book {
+                    Title = "みだれ髪",
+                    PublishedYear = 2000,
+                    Author = author1,
+                };
+                db.Books.Add(book1);
+                var author2 = db.Authors.Single(a => a.Name == "宮沢賢治");
+                var book2 = new Book {
+                    Title = "銀河鉄道の夜",
+                    PublishedYear = 1989,
+                    Author = author2,
+                };
+                db.Books.Add(book2);
+                db.SaveChanges();
+            }
+        }
+
+        private static void AddAuthors() {
+            using (var db = new BooksDbContext()) {
+                var author1 = new Author {
+                    Birthday = new DateTime(1878, 12, 7),
+                    Gender = "F",
+                    Name = "与謝野晶子",
+                };
+                db.Authors.Add(author1);
+                var author2 = new Author {
+                    Birthday = new DateTime(1896, 8, 27),
+                    Gender = "M",
+                    Name = "宮沢賢治",
+                };
+                db.Authors.Add(author2);
+                db.SaveChanges();
+            }
+        }
+
+        static void DisplayAllBooks() {
+            var books = GetBooks();
+            foreach (var book in books) {
+                Console.WriteLine($"{book.Title}{book.PublishedYear}");
+            }
             Console.ReadLine();
-            Console.WriteLine();
         }
 
         // List 13-5
@@ -38,6 +95,32 @@ namespace SampleEntityFramework {
                 };
                 db.Books.Add(book2);
                 db.SaveChanges();
+            }
+        }
+
+        static IEnumerable<Book> GetBooks() {
+            using (var db = new BooksDbContext()) {
+                return db.Books
+                         .Where(book => book.Author.Name.StartsWith("夏目"))
+                         .ToList();
+            }
+        }
+
+        private static void UpdateBook() {
+            using (var db = new BooksDbContext()) {
+                var book = db.Books.Single(x => x.Title == "銀河鉄道の夜");
+                book.PublishedYear = 2016;
+                db.SaveChanges();
+            }
+        }
+
+        private static void DeleteBook() {
+            using (var db = new BooksDbContext()) {
+                var book = db.Books.SingleOrDefault(x => x.Id == 10);
+                if (book != null) {
+                    db.Books.Remove(book);
+                    db.SaveChanges();
+                }
             }
         }
     }
