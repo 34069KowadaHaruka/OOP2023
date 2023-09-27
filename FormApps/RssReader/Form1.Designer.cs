8811844d827ae18fbb864113f546ba4742dc8ad4
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-09-27 11:26:35 +0900

    lbRssTitle Items.Addでタイトルを取得

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -24,13 +24,57 @@ namespace RssReader {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btGet = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(12, 17);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(334, 19);
+            this.tbUrl.TabIndex = 0;
+            // 
+            // btGet
+            // 
+            this.btGet.Location = new System.Drawing.Point(352, 17);
+            this.btGet.Name = "btGet";
+            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.TabIndex = 1;
+            this.btGet.Text = "取得";
+            this.btGet.UseVisualStyleBackColor = true;
+            this.btGet.Click += new System.EventHandler(this.btGet_Click);
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(12, 42);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(415, 400);
+            this.lbRssTitle.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(439, 450);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.btGet);
+            this.Controls.Add(this.tbUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.ListBox lbRssTitle;
     }
 }
 
