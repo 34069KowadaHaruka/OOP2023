d90683b3138ee38a376934522e20f1f34540edd9
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-10-10 09:55:55 +0900

    10章 演習問題4 模範解答

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -10,14 +10,10 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
-            var pattern = @"\s*=\s*";
-            string result = string.Join(Environment.NewLine, lines);
-            result.Replace(result, Regex.Replace(result, pattern, "="));
-            result.Replace(result, Regex.Replace(result, @"[Vv]ersion=""v4.0""", @"version=""v5.0"""));
-            
-            //var newLine = lines.
 
-            //File.WriteAllLines("sample.txt",newLine);
+            var newLine = lines.Select(s => Regex.Replace(s, @"\b(V|v)ersion\s*=\s*""v4.0""", @"version=""v5.0"""));
+
+            File.WriteAllLines("sample.txt",newLine);
 
              // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
