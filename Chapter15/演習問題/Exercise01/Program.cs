b62ce523bb2e38f5632e953e42dc47df01d5d32a
小和田 遥 <OIBC34069@ojs.ac.jp>
2023-10-11 12:06:21 +0900

    15章 演習問題 進捗(2問めまで)

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -25,12 +25,38 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2() {
+            var max = Library.Books.Max(b => b.Price);
+            var book = Library.Books
+                              .First(b => b.Price == max);
+            Console.WriteLine(book);
         }
 
         private static void Exercise1_3() {
+            var groups = Library.Books
+                                .GroupBy(b => b.PublishedYear);
+            int count = 0;
+            foreach (var g in groups) {
+                Console.Write($"{g.Key}年：");
+                foreach (var book in g) {
+                    count++;
+                }
+                Console.WriteLine(count+"冊");
+            }
+            //LINQを使おう
         }
 
         private static void Exercise1_4() {
+            var books = Library.Books
+                               .OrderByDescending(b => b.PublishedYear)
+                               .ThenByDescending(b => b.Price);
+            foreach (var book in books) {
+                Console.WriteLine("{0}年 {1}円 {2}({3})"
+                                    ,book.PublishedYear
+                                    ,book.Price
+                                    ,book.Title
+                                    ,Library.Categories.Where(c => c.Id.Equals(book.CategoryId)).Select(c => c.Name));
+            }
+            //カテゴリーネームが表示できてない
         }
 
         private static void Exercise1_5() {
